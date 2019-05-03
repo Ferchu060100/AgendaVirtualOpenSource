cbc9dd03c0a7293e145f542cf45c45d0efd52bea
Fernando Rey Arévalo Díaz <42156679+Ferchu060100@users.noreply.github.com>
2019-05-03 17:23:39 -0500

    Controller TipoDocumento

---
+++ AgendaVirtualApp/AgendaVirtualWeb/src/main/java/pe/agendavirtual/web/controller/TipoDocumentoController.java
@@ -0,0 +1,50 @@
+package pe.agendavirtual.web.controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import pe.agendavirtual.web.util.Message;
+import pe.agendavirtual.entity.TipoDocumento;
+import pe.agendavirtual.repository.TipoDocumentoRepository;
+
+@Named
+@ViewScoped
+public class TipoDocumentoController {
+	private static final long serialVersionUID = 1L;
+    @Inject	
+	TipoDocumentoRepository tipodocumentoRepo;
+	private TipoDocumento tipodocumento;
+    private List<TipoDocumento> tipodocumentos;
+    
+    @PostConstruct
+    public void init() {
+    
+    	cargarTipoDocumentos();
+    }
+	public void cargarTipoDocumentos(){
+		try {
+			this.setTipodocumentos(tipodocumentoRepo.listar());
+		} catch (Exception e) {
+			Message.messageError("Ocurrió un error al listar los tipo de documentos");
+		}
+		
+	}
+	public TipoDocumento getTipodocumento() {
+		return tipodocumento;
+	}
+	public void setTipodocumento(TipoDocumento tipodocumento) {
+		this.tipodocumento = tipodocumento;
+	}
+	public List<TipoDocumento> getTipodocumentos() {
+		return tipodocumentos;
+	}
+	public void setTipodocumentos(List<TipoDocumento> tipodocumentos) {
+		this.tipodocumentos = tipodocumentos;
+	}
+    
+    
+    
+}
